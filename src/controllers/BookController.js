1c56e7cb98a787868942b13c2e75d3c6e668cdb2
20020066 Phạm Quang Phong <20020066@vnu.edu.vn>
2022-05-13 15:35:41 +0700

    update by Po

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -1,18 +1,18 @@
 const connection = require('../connection_database/connector.js');
 
 class BookController {
-   // [GET] /books
-   show_all(req, res){
+  // [GET] /books
+  show_all(req, res) {
     let sql = `SELECT * FROM book`;
     connection.query(sql, function (error, results) {
       if (error) {
         return console.error(error.message);
       }
 
-      res.render("books/show_all", {books: results});
+      res.render('books/show_all', { books: results });
     });
   }
-  
+
   // [GET] /books/:slug
   show_detail(req, res) {
     let title = req.params.slug;
@@ -28,12 +28,12 @@ class BookController {
 
   // [GET] /books/create
   create(req, res) {
-    res.render('books/create')
+    res.render('books/create');
   }
 
   // [POST] /books/store
   store(req, res, next) {
-    res.json(req.body)
+    res.json(req.body);
   }
 }
 
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,19 +1,20 @@
-const connection = require("../connection_database/connector.js");
+const connection = require('../connection_database/connector.js');
 
 class HomeController {
-  // [GET] /
+  // [Get] /
   show(req, res, next) {
-    let sql = `SELECT * FROM book`;
+    let sql = `SELECT * FROM book LIMIT 12`;
     connection.query(sql, function (error, results, fields) {
       if (error) {
         return console.error(error.message);
       }
       
       const results_2d = [];
-      while(results.length) results_2d.push(results.splice(0,3));
+      while(results.length) results_2d.push(results.splice(0,4));
 
-      res.render("home", { books: results_2d });
+      res.render("home", {books_active: results_2d[0], books_item: results_2d.splice(1,3)});
     });
+    // console.log(books_item);
   }
 }
 
